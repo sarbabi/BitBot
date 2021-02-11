7cc2af285a05e86664a66dd1efe2ae6a4246b343
saeed arbabi <gsarbabi@gmail.com>
2021-02-11 12:11:54 +0100

    update get_now, utc+1h time_delta

--- dbtools/db.py
+++ dbtools/db.py
@@ -6,8 +6,8 @@ import localsettings
 
 
 db_credential = localsettings.db_credential
-engine = create_engine('mysql://{}:{}@localhost:{}/{}'.format(db_credential['username'],
-                                                              db_credential['password'],
+engine = create_engine('mysql://{}:{}@{}:{}/{}'.format(db_credential['username'],
+                                                              db_credential['password'], localsettings.db_credential['host'],
                                                               db_credential['port'],
                                                               db_credential['dbname']
                                                               )
